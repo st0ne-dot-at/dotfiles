09cca34030e364ae39021014b3df80d4d431ad53
Robert Steininger <github@st0ne.at>
2012-12-13 18:41:34 +0100

    added linking.sh

---
+++ linking.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+
+for file in $(ls -1d \.??* | grep -ve '^.git$'); do
+
+   if [ -L ~/$file ]; then
+      echo $file is a link ... nothing to do
+   elif [ ! -e ~/$file ]; then
+      echo ~/$file does not exist ... linking
+      ln -s $PWD/$file ~/$file
+
+   elif [ -d ~/$file -o -f $file ]; then
+      echo ~/$file is dir or file ... backup and linking
+      mv ~/$file ~/${file}.bak
+      ln -s $PWD/$file ~/$file
+   else
+      echo nothing to do for $file
+   fi
+done
+
+
+
+
